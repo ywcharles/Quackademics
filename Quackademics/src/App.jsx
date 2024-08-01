973e14066fb6e298073f34e11cfbc3464609341e
hoNATHAN <nathanlho@outlook.com>
2024-08-01 14:03:47 -0400

    add navbar to the app file

--- Quackademics/src/App.jsx
+++ Quackademics/src/App.jsx
@@ -4,10 +4,13 @@ import Pomodoro from "./routes/Pomodoro";
 import Profile from "./routes/Profile";
 import RubberDuck from "./routes/RubberDuck";
 import VoiceChannel from "./routes/VoiceChannel";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <div>website</div>
+      <Navbar />
       <BrowserRouter>
         <Routes>
           <Route path="dashboard/pomodoro" element={<Pomodoro />} />
