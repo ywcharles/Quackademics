9c9f35f2eccfb05ad826dde24d3d82e6b4b2a549
hoNATHAN <nathanlho@outlook.com>
2024-08-01 14:03:36 -0400

    add navbar functionality

--- Quackademics/src/components/Navbar.jsx
+++ Quackademics/src/components/Navbar.jsx
@@ -1,8 +1,35 @@
-import Box from "@mui/material/Box";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
 function Navbar() {
   return (
     <>
-      <Box></Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar>
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="nav"
+              sx={{ mr: 2 }}
+            >
+              <div>Icon</div>
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Test
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      {/* <Button variant="contained">Hello world</Button> */}
     </>
   );
 }
