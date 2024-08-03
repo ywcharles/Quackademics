987f7add6350bd8a154b5819aa8f4de61f5ab66f
hoNATHAN <nathanlho@outlook.com>
2024-08-02 23:07:34 -0400

    add new routes for the pages

--- Quackademics/src/App.jsx
+++ Quackademics/src/App.jsx
@@ -5,14 +5,17 @@ import Profile from "./routes/Profile";
 import RubberDuck from "./routes/RubberDuck";
 import VoiceChannel from "./routes/VoiceChannel";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
-      <div>website</div>
       <Navbar />
       <BrowserRouter>
         <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="dashboard/home" element={<Home />} />
           <Route path="dashboard/pomodoro" element={<Pomodoro />} />
           <Route path="dashboard/profile" element={<Profile />} />
           <Route path="dashboard/rubberduck" element={<RubberDuck />} />
