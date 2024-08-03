dbe560d5bcbc6b9b5bd7ab59da5dcde5728cd719
Nathan Ho <45053399+hoNATHAN@users.noreply.github.com>
2024-08-02 23:16:27 -0400

    Add skeleton website with empty pages (#8)
    * add blank pages
    
    * remove items in app file
    
    * reformat the routes
    
    * add route folder and format func
    
    * add empty routing for app page
    
    * add home page
    
    * add mui
    
    * navbar
    
    * add navbar functionality
    
    * add navbar to the app file
    
    * add mui libraries
    
    * implement navbar
    
    * add login page
    
    * add utils for the navbar to map
    
    * add new routes for the pages
    
    * add home page

--- Quackademics/src/App.jsx
+++ Quackademics/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Pomodoro from "./routes/Pomodoro";
+import Profile from "./routes/Profile";
+import RubberDuck from "./routes/RubberDuck";
+import VoiceChannel from "./routes/VoiceChannel";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="dashboard/home" element={<Home />} />
+          <Route path="dashboard/pomodoro" element={<Pomodoro />} />
+          <Route path="dashboard/profile" element={<Profile />} />
+          <Route path="dashboard/rubberduck" element={<RubberDuck />} />
+          <Route path="dashboard/voicechannel" element={<VoiceChannel />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Quackademics/src/routes/RubberDuck.jsx
@@ -0,0 +1,5 @@
+function RubberDuck() {
+  return <div>Rubber ducky</div>;
+}
+
+export default RubberDuck;
