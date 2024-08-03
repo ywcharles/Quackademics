938a4ec578a45024ff94b38624785b0926e7772f
hoNATHAN <nathanlho@outlook.com>
2024-08-02 23:07:17 -0400

    implement navbar

--- Quackademics/src/components/Navbar.jsx
+++ Quackademics/src/components/Navbar.jsx
@@ -3,33 +3,61 @@ import {
   Box,
   Button,
   IconButton,
+  MenuItem,
   Toolbar,
   Typography,
 } from "@mui/material";
+import { loginPage, navComponents } from "../util/Navbar.util";
 
 function Navbar() {
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar>
+        <AppBar sx={{ backgroundColor: "black" }}>
           <Toolbar>
             <IconButton
+              sx={{ mr: 2 }}
               size="large"
               edge="start"
               color="inherit"
               aria-label="nav"
-              sx={{ mr: 2 }}
             >
-              <div>Icon</div>
+              {/* TODO: add logo  */}
+              <img
+                style={{ width: "50px" }}
+                src="https://media.npr.org/assets/img/2013/06/04/ducky062way-cc767bbdf090919d6bbbed747dd19ab45e58e309.jpg?s=1100&c=50&f=jpeg"
+              />
             </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Test
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/dashboard/home"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              QUACKADEMICS
             </Typography>
-            <Button color="inherit">Login</Button>
+            {navComponents.map((page) => (
+              <MenuItem key={page}>
+                <Button href={page.href} color="inherit">
+                  {page.title}
+                </Button>
+              </MenuItem>
+            ))}
+            <Button href={loginPage.href} color="inherit">
+              Login
+            </Button>
           </Toolbar>
         </AppBar>
       </Box>
-      {/* <Button variant="contained">Hello world</Button> */}
     </>
   );
 }
