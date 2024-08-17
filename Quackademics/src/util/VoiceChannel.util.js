d2acc415367df15c5e605cfcffc9d2d9d8737456
hoNATHAN <nathanlho@outlook.com>
2024-08-17 19:10:19 -0400

    enum for ping cellular

---
+++ Quackademics/src/util/VoiceChannel.util.js
@@ -0,0 +1,11 @@
+import {
+  SignalCellularAlt1BarRounded,
+  SignalCellularAlt2BarRounded,
+  SignalCellularAltRounded,
+} from "@mui/icons-material";
+
+export const pingEnum = Object.freeze({
+  good: SignalCellularAltRounded,
+  fair: SignalCellularAlt2BarRounded,
+  bad: SignalCellularAlt1BarRounded,
+});
