02c7c388f34231a128385e3eea5833aec019bd93
hoNATHAN <nathanlho@outlook.com>
2024-08-17 20:22:29 -0400

    add more layouts

--- Quackademics/src/routes/VoiceChannel.jsx
+++ Quackademics/src/routes/VoiceChannel.jsx
@@ -1,11 +1,19 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import VoiceServerList from "../components/VoiceChannel/VoiceServerList";
+import { useVoiceChannelStore } from "../stores/VoiceChannelStore";
 
 const VoiceChannel = () => {
+  const serverName = useVoiceChannelStore((state) => state.serverName);
   return (
-    <Box sx={{ backgroundColor: "white" }} height={"100%"} width={"100%"}>
-      <VoiceServerList />
-    </Box>
+    <>
+      <Box>Join a Voice Channel to Study with Others!</Box>
+      <Box sx={{ backgroundColor: "black" }} height={"100%"} width={"100%"}>
+        <VoiceServerList />
+      </Box>
+      <Box>
+        <Button>Join {serverName}</Button>
+      </Box>
+    </>
   );
 };
 
