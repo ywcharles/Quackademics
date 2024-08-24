566e694b8ac7046f60a9aeb178a27305a9f136c8
hoNATHAN <nathanlho@outlook.com>
2024-08-24 17:18:53 -0400

    add notes path

--- Quackademics/src/routes/Notes.jsx
+++ Quackademics/src/routes/Notes.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import {
+  MDXEditor,
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  markdownShortcutPlugin,
+  MDXEditorMethods,
+  MDXEditorProps,
+} from "@mdxeditor/editor";
+import "@mdxeditor/editor/style.css";
 
 const Notes = () => {
-  return <div>Notes :3</div>;
+  return (
+    <h1>
+      Notes :3
+      <div>
+        MDEditor
+        <MDXEditor
+          plugin={[
+            headingsPlugin(),
+            listsPlugin(),
+            quotePlugin(),
+            thematicBreakPlugin(),
+            markdownShortcutPlugin(),
+          ]}
+          {...props}
+          ref={editorREf}
+        />
+      </div>
+    </h1>
+  );
 };
 
 export default Notes;
