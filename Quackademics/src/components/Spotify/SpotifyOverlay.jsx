206d17b61a10e6473f8391452378df86c3ff46f8
hoNATHAN <nathanlho@outlook.com>
2024-08-24 16:45:28 -0400

    add embed

--- Quackademics/src/components/Spotify/SpotifyOverlay.jsx
+++ Quackademics/src/components/Spotify/SpotifyOverlay.jsx
@@ -53,6 +53,17 @@ const SpotifyOverlay = () => {
 
   return (
     <>
+      {/* <iframe */}
+      {/*   style="border-radius:12px" */}
+      {/*   src="https://open.spotify.com/embed/playlist/0vvXsWCC9xrXsKd4FyS8kM?utm_source=generator" */}
+      {/*   width="100%" */}
+      {/*   height="352" */}
+      {/*   frameBorder="0" */}
+      {/*   allowfullscreen="" */}
+      {/*   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" */}
+      {/*   loading="lazy" */}
+      {/* ></iframe> */}
+      <div></div>
       <Button aria-describedby={id} onClick={handleSpotifyClick}>
         Spotify
       </Button>
