561cf5933b85e5b35a37804bfd96561403dcfb9c
hoNATHAN <nathanlho@outlook.com>
2024-08-25 13:48:26 -0400

    add theme wrap to main

--- Quackademics/src/App.jsx
+++ Quackademics/src/App.jsx
@@ -8,22 +8,31 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Assignments from "./routes/Assignments";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
   return (
     <>
-      <Navbar />
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/home" element={<Home />} />
-          <Route exact path="/flashcards" element={<Flashcards />} />
-          <Route exact path="/pomodoro" element={<Pomodoro />} />
-          <Route exact path="/assignments" element={<Assignments />} />
-          <Route exact path="/profile" element={<Profile />} />
-          <Route exact path="/rubberduck" element={<RubberDuck />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/home" element={<Home />} />
+            <Route exact path="/flashcards" element={<Flashcards />} />
+            <Route exact path="/pomodoro" element={<Pomodoro />} />
+            <Route exact path="/assignments" element={<Assignments />} />
+            <Route exact path="/profile" element={<Profile />} />
+            <Route exact path="/rubberduck" element={<RubberDuck />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
 }
