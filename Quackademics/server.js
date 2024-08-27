339c7bca6223b120920271c74a87a8cc0e3035e0
hoNATHAN <nathanlho@outlook.com>
2024-08-27 16:02:27 -0400

    create server js

---
+++ Quackademics/server.js
@@ -0,0 +1,26 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import express from "express";
+import { createServer as createViteServer } from "vite";
+
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+async function createServer() {
+  const app = express();
+
+  const vite = await createViteServer({
+    server: { middlewareMode: true },
+    appType: "custom",
+  });
+
+  app.use(vite.middlewares);
+
+  // app.use("*", async (req, res) => {
+  //   // serve index.html - we will tackle this next
+  // });
+
+  app.listen(80);
+}
+
+createServer();
