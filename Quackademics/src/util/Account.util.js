75821bafddd3d3c596b7053e217dd18114539bb7
hoNATHAN <nathanlho@outlook.com>
2024-08-27 18:23:33 -0400

    add pfp utils

---
+++ Quackademics/src/util/Account.util.js
@@ -0,0 +1,8 @@
+export const profilePics = {
+  pfp1: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
+  pfp2: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
+  pfp3: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
+  pfp4: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
+  pfp5: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
+  pfp6: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
+};
