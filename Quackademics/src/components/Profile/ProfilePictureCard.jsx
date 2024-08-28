9039cac0e6ab2aa9d352628babd667ce958f55b6
hoNATHAN <nathanlho@outlook.com>
2024-08-27 23:23:27 -0400

    add profile picture card for listing

--- Quackademics/src/components/Profile/ProfilePictureCard.jsx
+++ Quackademics/src/components/Profile/ProfilePictureCard.jsx
@@ -1,5 +1,23 @@
+import { Card, ImageList, ImageListItem } from "@mui/material";
+import { profilePics } from "../../util/Account.util";
+
 const ProfilePictureCard = () => {
-  return <div>Profile Set up</div>;
+  return (
+    <Card>
+      Select a profile picture!
+      <ImageList
+        sx={{ width: "fit-content", height: "fit-content" }}
+        cols={6}
+        rowHeight={164}
+      >
+        {profilePics.map((pfp) => (
+          <ImageListItem sx={{ width: "90px" }} key={pfp.pfpNum}>
+            <img src={pfp.src} />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Card>
+  );
 };
 
 export default ProfilePictureCard;
--- Quackademics/src/util/Account.util.js
+++ Quackademics/src/util/Account.util.js
@@ -1,8 +1,26 @@
-export const profilePics = {
-  pfp1: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
-  pfp2: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
-  pfp3: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
-  pfp4: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
-  pfp5: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
-  pfp6: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
-};
+export const profilePics = [
+  {
+    pfpNum: 1,
+    src: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
+  },
+  {
+    pfpNum: 2,
+    src: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
+  },
+  {
+    pfpNum: 3,
+    src: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
+  },
+  {
+    pfpNum: 4,
+    src: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
+  },
+  {
+    pfpNum: 5,
+    src: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
+  },
+  {
+    pfpNum: 6,
+    src: "https://i.ebayimg.com/images/g/HhkAAOSwsR5fabI-/s-l1200.jpg",
+  },
+];
