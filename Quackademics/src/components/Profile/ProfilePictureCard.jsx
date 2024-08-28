ffaabb6cdd3e847af1aaad17b6704010841fcbb4
Kiki Lin <kikilin512@gmail.com>
2024-08-28 00:45:53 -0400

    implemented selecting avatar from selected image list

--- Quackademics/src/components/Profile/ProfilePictureCard.jsx
+++ Quackademics/src/components/Profile/ProfilePictureCard.jsx
@@ -1,18 +1,44 @@
+import React, { useState } from "react";
 import { Card, ImageList, ImageListItem } from "@mui/material";
 import { profilePics } from "../../util/Account.util";
 
-const ProfilePictureCard = () => {
+const ProfilePictureCard = ({ setProfilePicture }) => {
+  const [selectedImage, setSelectedImage] = useState("");
+
+  const handleImageSelect = (src) => {
+    setSelectedImage(src);
+    setProfilePicture(src); 
+  };
+  
   return (
-    <Card>
-      Select a profile picture!
+    <Card sx={{ padding: 2 }}>
+      <div>Select a profile picture!</div>
       <ImageList
-        sx={{ width: "fit-content", height: "fit-content" }}
-        cols={6}
+        sx={{ height: "fit-content", width: "100%"}}
+        cols={5} 
         rowHeight={164}
       >
         {profilePics.map((pfp) => (
-          <ImageListItem sx={{ width: "90px" }} key={pfp.pfpNum}>
-            <img src={pfp.src} />
+          <ImageListItem
+            key={pfp.pfpNum}
+            sx={{
+              width: "90px",
+              cursor: "pointer",
+              border: selectedImage === pfp.src ? "3px solid blue" : "none",
+              borderRadius: "8px", 
+              overflow: "hidden", 
+            }}
+            onClick={() => handleImageSelect(pfp.src)}
+          >
+            <img
+              src={pfp.src}
+              alt={`Profile ${pfp.pfpNum}`}
+              style={{
+                width: "100%",
+                height: "auto", 
+                display: "block", 
+              }}
+            />
           </ImageListItem>
         ))}
       </ImageList>
