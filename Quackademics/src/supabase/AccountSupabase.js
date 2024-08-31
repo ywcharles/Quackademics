b4015efefc78967e001d0b47d6e4d9f2c1abc396
hoNATHAN <nathanlho@outlook.com>
2024-08-31 15:21:53 -0400

    migrate bcrypt js helper func

--- Quackademics/src/supabase/AccountSupabase.js
+++ Quackademics/src/supabase/AccountSupabase.js
@@ -1,4 +1,10 @@
 import supabase from "../libs/supabaseAdmin";
+import bcryptjs from "bcryptjs";
+
+export const hashPassword = async (password) => {
+  const salt = await bcryptjs.genSalt(10);
+  return await bcryptjs.hash(password, salt);
+};
 
 const getCurrentTimestamp = () => {
   return new Date().toISOString().slice(0, 19).replace("T", " ");
@@ -20,6 +26,24 @@ export const addUser = async (username, email, password, profilePicture) => {
   if (error) return error;
 };
 
+export const checkIfUserExists = async (username) => {
+  const { data, error } = await supabase
+    .from("users")
+    .select()
+    .eq("username", "asdffffffffff");
+
+  console.log("data", data);
+  console.log(data.length !== 0);
+  return data.length !== 0;
+};
+
+export const validatePassword = async (password) => {
+  const { data, error } = await supabase
+    .from("users")
+    .select()
+    .eq("password", "adsf");
+};
+
 export const signInUser = async (user_id, password) => {
-  const { error } = await supabase.from("users").update({ user_id: user_id });
+  const { error } = await supabase.from("users");
 };
