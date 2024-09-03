7daba9099ca403e374433334975d266d759c8282
hoNATHAN <nathanlho@outlook.com>
2024-09-03 16:58:16 -0400

    add more store states to track user

--- Quackademics/src/stores/UserSessionStore.js
+++ Quackademics/src/stores/UserSessionStore.js
@@ -6,10 +6,21 @@ export const useUserSessionStore = create(
     (set) => ({
       userId: null,
       setUserId: (userId) => set(() => ({ userId: userId })),
+
+      username: null,
+      setUsername: (username) => set(() => ({ username: username })),
+
+      profilePicture: null,
+      setProfilePicture: (profilePicture) =>
+        set(() => ({ profilePicture: profilePicture })),
+
+      loginSuccess: false,
+      setLoginSuccess: (loginSuccess) =>
+        set(() => ({ loginSuccess: loginSuccess })),
     }),
     {
-      name: "user-session-store", 
-      getStorage: () => localStorage, 
-    }
-  )
+      name: "user-session-store",
+      getStorage: () => localStorage,
+    },
+  ),
 );
