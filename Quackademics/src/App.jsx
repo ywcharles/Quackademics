ee3bb94becf77069fa3e5e63b731e7399d671ed5
hoNATHAN <nathanlho@outlook.com>
2024-09-03 16:58:22 -0400

    add snackbar on top layer

--- Quackademics/src/App.jsx
+++ Quackademics/src/App.jsx
@@ -9,10 +9,41 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Assignments from "./routes/Assignments";
 import Notes from "./routes/Notes";
+import { Snackbar } from "@mui/material";
+import { useUserSessionStore } from "./stores/UserSessionStore";
 
 function App() {
+  const username = useUserSessionStore((state) => state.username);
+  const loginSuccess = useUserSessionStore((state) => state.loginSuccess);
+  const profilePicture = useUserSessionStore((state) => state.profilePicture);
+
+  const setLoginSuccess = useUserSessionStore((state) => state.setLoginSuccess);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setLoginSuccess(false);
+  };
+
   return (
     <>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        open={loginSuccess}
+        onClose={handleClose}
+        autoHideDuration={2000}
+        // TransitionComponent={<Slide direction="up" />}
+        message={
+          <h3>
+            <img
+              src={profilePicture}
+              style={{ width: "50px", height: "50px" }}
+            />
+            Welcome, {username}!
+          </h3>
+        }
+      />
       <Navbar />
       <BrowserRouter basename="/">
         <Routes>
