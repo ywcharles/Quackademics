5b9a23791f9ac06cdf2f457cfca504e35f3eb438
hoNATHAN <nathanlho@outlook.com>
2024-09-04 16:17:53 -0400

    set up supabase calls

---
+++ Quackademics/src/supabase/ProfileSupabase.js
@@ -0,0 +1,9 @@
+export const editProfilePicture = () => {};
+
+export const editBio = () => {};
+
+export const editStudy = () => {};
+
+export const editGraduationYear = () => {};
+
+export const editStartYear = () => {};
