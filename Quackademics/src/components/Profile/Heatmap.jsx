09bf69637e0fe5799ad8e3114b8b1e1849a4e262
navingeorge <navin.george10@gmail.com>
2024-09-05 11:46:29 -0400

    Heatmap skeleton

--- Quackademics/src/components/Profile/Heatmap.jsx
+++ Quackademics/src/components/Profile/Heatmap.jsx
@@ -1,5 +1,17 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { HeatMapGrid } from "react-grid-heatmap";
+
+const xLabels = new Array(24).fill(0).map((_, i) => `${i}`);
+const yLabels = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+const data = new Array(yLabels.length)
+  .fill(0)
+  .map(() =>
+    new Array(xLabels.length)
+      .fill(0)
+      .map(() => Math.floor(Math.random() * 5 + 5))
+  );
 
 const Heatmap = () => {
   return (
@@ -8,16 +20,41 @@ const Heatmap = () => {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        backgroundColor: "#615f5f",
-        color: "white",
+        backgroundColor: "#f0f0f0",
+        color: "black",
         borderRadius: 2,
         width: "100%",
-        height: "60%"
+        height: "auto",
+        padding: 2,
       }}
     >
-      Heatmap
+      <HeatMapGrid
+        data={data}
+        xLabels={xLabels}
+        yLabels={yLabels}
+        cellRender={(x, y, value) => (
+          <div title={`Pos(${x}, ${y}) = ${value}`}>{value}</div>
+        )}
+        xLabelsStyle={(index) => ({
+          color: index % 2 ? "transparent" : "#777",
+          fontSize: ".65rem",
+        })}
+        yLabelsStyle={() => ({
+          fontSize: ".65rem",
+          textTransform: "uppercase",
+          color: "#777",
+        })}
+        cellStyle={(_x, _y, ratio) => ({
+          background: `rgb(12, 160, 44, ${ratio})`,
+          fontSize: ".7rem",
+          color: `rgb(0, 0, 0, ${ratio / 2 + 0.4})`,
+        })}
+        cellHeight="1.5rem"
+        xLabelsPos="bottom"
+        onClick={(x, y) => alert(`Clicked (${x}, ${y})`)}
+      />
     </Box>
-  )
-}
+  );
+};
 
 export default Heatmap;
