f8191d5a7879c2ef170e8be7b0521e45a05ab924
cyw37 <cyw37@drexel.edu>
2024-09-05 15:48:17 -0400

    Navbar Content Right Aligned

--- Quackademics/src/components/Navbar.jsx
+++ Quackademics/src/components/Navbar.jsx
@@ -33,12 +33,12 @@ function Navbar() {
               color="inherit"
               aria-label="nav"
             >
-              {/* TODO: add logo  */}
               <img
                 style={{ width: "50px" }}
                 src="https://media.npr.org/assets/img/2013/06/04/ducky062way-cc767bbdf090919d6bbbed747dd19ab45e58e309.jpg?s=1100&c=50&f=jpeg"
               />
             </IconButton>
+
             <Typography
               variant="h6"
               noWrap
@@ -56,13 +56,18 @@ function Navbar() {
             >
               QUACKADEMICS
             </Typography>
-            {navComponents.map((page) => (
-              <MenuItem key={page}>
-                <Button href={page.href} color="inherit">
-                  {page.title}
-                </Button>
-              </MenuItem>
-            ))}
+
+            <Box sx={{ flexGrow: 1 }} />
+
+            {userId &&
+              navComponents.map((page) => (
+                <MenuItem key={page}>
+                  <Button href={page.href} color="inherit">
+                    {page.title}
+                  </Button>
+                </MenuItem>
+              ))}
+
             {userId === null || userId === "" ? (
               <Button href={loginPage.href} color="inherit">
                 Login
@@ -72,8 +77,8 @@ function Navbar() {
                 Sign Out
               </Button>
             )}
-            <Box sx={{ flexGrow: 1 }} />
-            <NotificationPopover />
+
+            {userId && <NotificationPopover />}
           </Toolbar>
         </AppBar>
       </Box>
